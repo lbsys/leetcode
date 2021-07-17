cbfeae618f7cdc9c3688c6f9c29321d456ab650b
lbsys <2865866423@qq.com>
2021-07-17 15:08:35 +0800

    最长不重复子串

---
+++ src/leetcode/editor/cn/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,81 @@
+//给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。 
+//
+// 
+//
+// 示例 1: 
+//
+// 
+//输入: s = "abcabcbb"
+//输出: 3 
+//解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+// 
+//
+// 示例 2: 
+//
+// 
+//输入: s = "bbbbb"
+//输出: 1
+//解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+// 
+//
+// 示例 3: 
+//
+// 
+//输入: s = "pwwkew"
+//输出: 3
+//解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+//     请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+// 
+//
+// 示例 4: 
+//
+// 
+//输入: s = ""
+//输出: 0
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 0 <= s.length <= 5 * 104 
+// s 由英文字母、数字、符号和空格组成 
+// 
+// Related Topics 哈希表 双指针 字符串 Sliding Window 
+// 👍 5105 👎 0
+
+/**
+ * @author yitiaoIT
+ */
+package leetcode.editor.cn;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class LongestSubstringWithoutRepeatingCharacters {
+    public static void main(String[] args) {
+        Solution solution = new LongestSubstringWithoutRepeatingCharacters().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+
+        int maxLength=0;
+        int start = 0,end=0;
+        HashMap<Character, Integer> map = new HashMap<>();
+        char[] chars = s.toCharArray();
+        while(end<chars.length){
+            if (map.containsKey(chars[end])){
+                start=Math.max(map.get(chars[end]),start);
+            }
+            maxLength=Math.max(maxLength,end-start+1);
+            map.put(chars[end],end+1);
+            end++;
+        }
+        return maxLength;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
